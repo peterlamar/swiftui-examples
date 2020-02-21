359da3125bf45d74fee61fb2f1d1d39587477704
Peter Lamar <peterrlamar@gmail.com>
2020-02-21 10:52:31 -0500

    Add example for UIKit

---
+++ UIKitBasics/UIKitBasics/README.md
@@ -0,0 +1,5 @@
+#  UIKit Basics
+
+Since SwiftUI isn't fully baked yet, one must be capable with UIKit as well.
+
+Tutorial followed [here](https://www.youtube.com/watch?v=jniJeamcIUU)
---
+++ UIKitBasics/UIKitBasics/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  UIKitBasics
+//
+//  Created by Peter Lamar on 2/20/20.
+//  Copyright © 2020 Peter Lamar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
