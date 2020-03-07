e44a6e59c0a0f121d6bf7710d6789cc3ee33dd2e
Peter Lamar <peterrlamar@gmail.com>
2020-03-07 12:05:50 -0500

    UI Kit example

---
+++ UIKitNotification/NtnExl/NotificationViewController.swift
@@ -0,0 +1,42 @@
+//
+//  NotificationViewController.swift
+//  NtnExl
+//
+//  Created by Peter Lamar on 2/27/20.
+//  Copyright © 2020 Peter Lamar. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+    
+    @IBOutlet weak var SubLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+
+    // Init
+    func didReceive(_ notification: UNNotification) {
+        self.SubLabel?.text = "Short impressive subtitle"
+    }
+
+    // This runs when button is pressed
+    func didReceive(_ response: UNNotificationResponse, completionHandler _:
+        (UNNotificationContentExtensionResponseOption) -> Void) {
+        
+        print("code ran")
+        
+        guard let currentActions = extensionContext?.notificationActions else { return }
+
+        if response.actionIdentifier == "yes-action" {
+            print("All your money are belong to us")
+        }
+        if response.actionIdentifier == "no-action" {
+            print("Resistence is futile")
+        }
+    }
+}
---
+++ UIKitNotification/UIKitNotification/AppDelegate.swift
@@ -0,0 +1,41 @@
+//
+//  AppDelegate.swift
+//  UIKitNotification
+//
+//  Created by Peter Lamar on 2/27/20.
+//  Copyright © 2020 Peter Lamar. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+
+        // Request user's permission to send notifications.
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+            if granted {
+                print("Notifications permission granted.")
+            } else {
+                print("Notifications permission denied because: \(error?.localizedDescription).")
+            }
+        }
+
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options _: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_: UIApplication, didDiscardSceneSessions _: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+}
