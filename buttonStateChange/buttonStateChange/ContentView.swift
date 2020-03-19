2a24b2ffd313b34d00775a23b88ee7be9d1ef835
Peter Lamar <peterrlamar@gmail.com>
2020-03-19 15:25:13 -0400

    Add button state change example

---
+++ buttonStateChange/buttonStateChange/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  buttonStateChange
+//
+//  Created by Peter Lamar on 3/19/20.
+//  Copyright © 2020 Peter Lamar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var sColor = Color.black
+    
+     var body: some View {
+
+
+        return VStack {
+            Button("Tap Me!") {
+                print("button pressed!")
+
+                self.sColor = Color.red
+
+            }.buttonStyle(MyButtonStyle(color: sColor))
+        }
+
+    }
+}
+
+struct MyButtonStyle: ButtonStyle {
+    var color: Color
+    
+    public func makeBody(configuration: MyButtonStyle.Configuration) -> some View {
+                
+        print ("inside button style")
+        print(color)
+        
+        return configuration.label
+            .foregroundColor(.white)
+            .padding(15)
+            .background(RoundedRectangle(cornerRadius: 5).fill(color))
+            .compositingGroup()
+            .shadow(color: .black, radius: 3)
+            .opacity(configuration.isPressed ? 0.5 : 1.0)
+            .scaleEffect(configuration.isPressed ? 0.8 : 1.0)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
