c39466041337303286088d79f0e41182f0738b4a
plamar <plamar@hubspot.com>
2021-03-21 21:01:39 -0400

    Add new exclusiv

---
+++ exclusiveLists/Shared/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Peter Lamar on 3/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    struct FileItem: Hashable, Identifiable, CustomStringConvertible {
+        var id: Self { self }
+        var name: String
+        var children: [FileItem]? = nil
+        var description: String {
+            switch children {
+            case nil:
+                return "📄 \(name)"
+            case .some(let children):
+                return children.isEmpty ? "📂 \(name)" : "📁 \(name)"
+            }
+        }
+    }
+    let fileHierarchyData: [FileItem] = [
+      FileItem(name: "users", children:
+        [FileItem(name: "user1234", children:
+          [FileItem(name: "Photos", children:
+            [FileItem(name: "photo001.jpg"),
+             FileItem(name: "photo002.jpg")]),
+           FileItem(name: "Movies", children:
+             [FileItem(name: "movie001.mp4")]),
+              FileItem(name: "Documents", children: [])
+          ]),
+         FileItem(name: "newuser", children:
+           [FileItem(name: "Documents", children: [])
+           ])
+        ]),
+        FileItem(name: "private", children: nil)
+    ]
+    var body: some View {
+        List(fileHierarchyData, children: \.children) { item in
+            Text(item.description)
+        }
+    }
+}
---
+++ exclusiveLists/Shared/exclusiveListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  exclusiveListsApp.swift
+//  Shared
+//
+//  Created by Peter Lamar on 3/21/21.
+//
+
+import SwiftUI
+
+@main
+struct exclusiveListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
