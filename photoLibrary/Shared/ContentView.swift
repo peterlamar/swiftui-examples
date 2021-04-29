d3be3f8944fc0452b7eca569a1cad9eea0006f27
plamar <plamar@hubspot.com>
2021-04-29 08:12:32 -0400

    Add PhotoLibrary example

---
+++ photoLibrary/Shared/ContentView.swift
@@ -0,0 +1,89 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Peter Lamar on 4/29/21.
+//
+// Based on https://www.appcoda.com/swiftui-camera-photo-library/
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var isShowPhotoLibrary = false
+    @State private var image = UIImage()
+
+    var body: some View {
+        VStack {
+            Image(uiImage: self.image)
+                .resizable()
+                .scaledToFill()
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .edgesIgnoringSafeArea(.all)
+
+            Button(action: {
+                self.isShowPhotoLibrary = true
+            }) {
+                HStack {
+                    Image(systemName: "photo")
+                        .font(.system(size: 20))
+
+                    Text("Photo library")
+                        .font(.headline)
+                }
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 50)
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(20)
+                .padding(.horizontal)
+            }
+        }
+        .sheet(isPresented: $isShowPhotoLibrary) {
+            ImagePicker(sourceType: .photoLibrary, selectedImage: self.$image)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct ImagePicker: UIViewControllerRepresentable {
+    var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    @Binding var selectedImage: UIImage
+    @Environment(\.presentationMode) private var presentationMode
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        let imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = false
+        imagePicker.delegate = context.coordinator
+
+        return imagePicker
+        // Return an instance of UIImagePickerController
+    }
+
+    func updateUIViewController(_: UIImagePickerController, context _: UIViewControllerRepresentableContext<ImagePicker>) {}
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    final class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+     
+        var parent: ImagePicker
+     
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+     
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+     
+            if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                parent.selectedImage = image
+            }
+     
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
---
+++ photoLibrary/Shared/photoLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  photoLibraryApp.swift
+//  Shared
+//
+//  Created by Peter Lamar on 4/29/21.
+//
+
+import SwiftUI
+
+@main
+struct photoLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
